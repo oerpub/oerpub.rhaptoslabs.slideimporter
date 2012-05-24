de356e6b5466d6973829b673cdafc795bbe027ed
Saket Choudhary <saketkc@gmail.com>
2012-05-24 22:26:01 +0100

    Modified GooglePresentaionUploader to allow publishing on the web automatically

--- src/oerpub/rhaptoslabs/slideimporter/google_presentations.py
+++ src/oerpub/rhaptoslabs/slideimporter/google_presentations.py
@@ -1,3 +1,6 @@
+import gdata.docs.data
+import gdata.docs.client
+import gdata.docs.service
 import sys
 import gdata.client
 import gdata.docs.client
@@ -6,24 +9,49 @@ import stat
 import mimetools, mimetypes
 import time
 import atom
-fd = open("/home/saket/Downloads/presentation.ppt","r")
-file_size = os.fstat(fd.fileno())[stat.ST_SIZE]
-filename = fd.name.split('/')[-1]
-file_type = mimetypes.guess_type(filename)[0] or 'application/octet-stream'
-#print contenttype
-#print fd.name
-docsclient = gdata.docs.client.DocsClient()
-username = ''
-password = ''
-docsclient.ClientLogin(username, password, docsclient.source)
-uri = 'https://docs.google.com/feeds/upload/create-session/default/private/full'
-collection = 'test'
-resources = docsclient.GetAllResources(uri='https://docs.google.com/feeds/default/private/full/-/folder?title=' + collection + '&title-exact=true')
-uri = resources[0].get_resumable_create_media_link().href
-#uri += '?convert=false'
-t1 = time.time()
-uploader = gdata.client.ResumableUploader(docsclient, fd, file_type, file_size, chunk_size=1048576, desired_class=gdata.data.GDEntry)
-new_entry = uploader.UploadFile(uri, entry=gdata.data.GDEntry(title=atom.data.Title(text=os.path.basename(fd.name))))
+class GooglePresentationUploader:
+	def __init__(self,username,password):
+		self.client = gdata.docs.client.DocsClient(source='connexions')
+		self.client.api_version = "3"
+		self.client.ssl = True
+		self.client.ClientLogin(username, password, self.client.source)
+	
+	def upload(self,filepath):
+		self.fd = open(filepath,"r")
+		self.file_size = os.fstat(self.fd.fileno())[stat.ST_SIZE]
+		self.filename = self.fd.name.split('/')[-1]
+		self.file_type = mimetypes.guess_type(self.filename)[0] or 'application/octet-stream'
+		self.media = gdata.data.MediaSource()
+		self.media.SetFileHandle(filepath, self.file_type)
+		self.new_presentation_resource = gdata.docs.data.Resource(filepath, self.filename)
+		self.new_presentation = self.client.CreateResource(self.new_presentation_resource, create_uri=gdata.docs.client.RESOURCE_UPLOAD_URI, media=self.media)
 
+# Is there a better way to get the reource id ?, This originaly returnds  'presentaion : id'
+	def get_resource_id(self):
+		self.resource_id = self.new_presentation.resource_id.text.split(':')[1] 
+		return self.resource_id
 
+	def get_revision_feeds(self):
+		self.resource = self.client.GetResourceById(self.get_resource_id())
+		self.revision_feeds = self.client.GetRevisions(self.resource)
 
+# We havn't edited the PPT so just fetching the first revision
+	def get_first_revision_feed(self):
+		self.get_revision_feeds()
+		self.entry = self.revision_feeds.entry[0]
+		return self.entry
+	def publish_presentation_on_web(self):
+		self.entry.publish = gdata.docs.data.Publish(value='true')
+		self.entry.publish_auto = gdata.docs.data.PublishAuto(value='true')
+		self.update_entry = self.client.update(self.entry,force=True)
+	def get_embed_url(self):
+		s = """<iframe src="https://docs.google.com/presentation/embed?id="""+self.resource_id+"""&start=false&loop=false&delayms=3000" frameborder="0" width="1058" height="823" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>"""
+		print s
+
+if __name__ == "__main__":
+	a = GooglePresentationUploader("saketkc","howdoyoudothis1314.")
+	a.upload("/home/saket/Downloads/presentation.ppt")
+	a.get_resource_id()
+	a.get_first_revision_feed()
+	a.publish_presentation_on_web()
+	a.get_embed_url()
